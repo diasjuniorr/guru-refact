7e784cef3dc4bd37ec5c08a5a2aaaa688cbe7fb3
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-08-10 10:27:06 +0200

    add flatten function

--- flatten.js
+++ flatten.js
@@ -2,7 +2,26 @@ const assert = require("assert");
 
 let arr = [1, [2, [3]], 4];
 
-const flatten = (arr) => {};
+const flatten = (arr) => {
+  let hasArray = false;
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result.push(...arr[i]);
+      hasArray = true;
+      continue;
+    }
+
+    result.push(arr[i]);
+  }
+
+  if (hasArray) {
+    return flatten(result);
+  }
+
+  return result;
+};
 
 const expected = [1, 2, 3, 4];
 assert.deepEqual(flatten(arr), expected);
